550e05976e22fa420113d4af373d09043dda484f
Juancho10 <juanitolizarazu18@gmail.com>
2025-11-28 19:06:54 -0500

    ultimos cambios realizados

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   devToolbar:{
     enabled: false,
-  }
+  },
+
+  integrations: [vue()]
 });
